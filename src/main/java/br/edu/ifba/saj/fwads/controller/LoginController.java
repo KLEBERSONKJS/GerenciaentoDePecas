b312324cbb3a85496e9cb4c784ad3b33215543c8
KLEBERSONKJS <165091663+KLEBERSONKJS@users.noreply.github.com>
2024-09-19 16:56:06 -0300

    up

--- src/main/java/br/edu/ifba/saj/fwads/controller/LoginController.java
+++ src/main/java/br/edu/ifba/saj/fwads/controller/LoginController.java
@@ -11,9 +11,9 @@ import br.edu.ifba.saj.fwads.Usuario;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-import javafx.scene.control.Alert.AlertType;
 
 public class LoginController {
 
