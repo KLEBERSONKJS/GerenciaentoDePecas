e35632863e1a15b36ff6aff91027e32f571b65c6
KLEBERSONKJS <20231tadssaj0010@ifba.edu.br>
2024-09-22 14:42:20 -0300

    up

--- src/main/java/br/edu/ifba/saj/fwads/Estoque.java
+++ src/main/java/br/edu/ifba/saj/fwads/Estoque.java
@@ -12,42 +12,78 @@ public class Estoque {
     public static ObservableList<Peca> pecas = FXCollections.observableArrayList(); 
     public static ObservableList<Logista> logitas = FXCollections.observableArrayList(); 
     public static ObservableList<Marca> marcas = FXCollections.observableArrayList(); 
+    public static ObservableList<Tipo> tipos = FXCollections.observableArrayList();
+
     
+    //REGISTRAR MARCA DE PECA
+    public static void novaMarca(String nome){
+        marcas.add(new Marca(nome));
+    }
+    
+    //CADASTRAR UMA PECA
+    public static void novaPeca(Marca marca, Tipo tipo, String modelo, double custo, double valorVenda, int qnt){
+        pecas.add(new Peca( marca, tipo, modelo, custo, valorVenda, qnt));
+        
+    }
 
+    //CADASTRAR UM LOGISTA
+    public static void novoLogista(String nome){
+        logitas.add(new Logista(nome));
+    }
 
+    //CADASTRAR UM NOVO TIPO DE PECA
+    public static void novoTipo(String nome){
+        tipos.add(new Tipo(nome));
+    } 
+
+    //QUANTIDADE DE MERCADORIA
+    public static int qntPecas(){
+        int qntPecas = 0;
+        for (Peca p : pecas) {
+            qntPecas =+ p.getQnt();
+        }
+        return qntPecas;
+    }
+
+    //QUANTIDADE DE PECAS
+    public static int qntModelos(){
+        return pecas.size(); 
+    }
+
+    //VENDER PECA DESCONTA DO ESTOQUE E ADD AO LOGISTA COMPRADOR
     public void vender(Peca peca, Logista logista , int valor, int qnt){
         peca.setQnt(peca.getQnt() - 1);
 
     }
 
 
-    public static void novaPeca(Marca marca, Tipo tipo, String modelo, double custo, double valorVenda, int qnt){
-        Peca novaPeca = new Peca( marca, tipo, modelo, custo, valorVenda, qnt);
-        pecas.add(novaPeca);
-        
-    }
-
+    //ACRESENTAR PECAS A UM ITEM JA EXISTENTE
     public void setQuantidadepcs(Peca peca, int adicional){
         peca.setQnt(peca.getQnt() + adicional);
     }
 
-    public static void novoLogista(String nome){
-        Logista novoLogista = new Logista(nome);
-        logitas.add(novoLogista);
-    }
 
-
+    //TODAS AS PECAS DA LISTA
     public static ObservableList<Peca> getPecas() {
         return pecas;
     }
 
-
+    //TODOS OS LOGISTAS
     public static ObservableList<Logista> getLogitas() {
         return logitas;
     }
 
-    public static void novaMarca(String nome){
-        Marca novMarca = new Marca(nome);
-        marcas.add(novMarca);
+    //RETORNA A PECA PESQUISADA
+    public static Peca pesquisar(String nome){
+        Peca pesquisada = null;
+        if(pecas.isEmpty()){
+            for (Peca p : pecas) {
+                if(p.getModelo().equalsIgnoreCase(nome)){
+                break;
+                }
+            }
+        }    
+        return pesquisada;
     }
+
 }
